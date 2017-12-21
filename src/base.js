67c99208fb176ac93fb2b09bca1d34b0e3f675d4
Steve Frost <sfrost@nerdery.com>
2017-12-21 10:40:18 -0600

    removed solutions & simplified file structure

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyCjGN4PDu_C5BJcZI9kOay06rFdA4HYEj4",
+    authDomain: "catch-of-the-day-9bb98.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-9bb98.firebaseio.com",
+})
+
+export default base
